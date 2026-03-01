96ff84190633b875d5c945b88677d3043fba27a6
izhank216 <izhank216@gmail.com>
2026-03-01 19:57:36 +0500

    not again

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import Memory from "./Memory.js"
 import CPU from "./CPU.js"
-import GPU from "./GPU.js"
 
 class Calcmachine {
 
   constructor() {
     this.memory = new Memory()
     this.cpu = new CPU(this.memory)
-    this.gpu = new GPU(this.memory)
     this.debug = false
   }
 
   run(program) {
     this.memory.clear()
-
     this.cpu.stack = []
     this.cpu.callStack = []
-    this.cpu.pc = 0
-    this.cpu.running = false
-
-    this.gpu.pc = 0
-    this.gpu.running = false
-    this.gpu.registers.fill(0)
-    this.gpu.vram.fill(0)
-
     this.cpu.load(program)
 
     if (Calcmachine.debugMode && Calcmachine.debugMode.enabled) {
@@ -31,20 +20,21 @@ class Calcmachine {
       console.log("Calcmachine debug mode ENABLED")
     }
 
-    if (this.debug) {
-      this.cpu.stepHook = (cpu) => {
-        console.log(`CPU PC: ${cpu.pc}`)
-      }
-    }
+    const stepHook = this.debug ? (op, pc, stack) => {
+      console.log(`PC: ${pc}, OP: ${op}, STACK: [${stack.join(", ")}]`)
+    } : null
+
+    if (stepHook) this.cpu.stepHook = stepHook
 
     return this.cpu.run()
   }
 
 }
 
+
 Calcmachine.debugMode = {
   enabled: false,
-  enable() { this.enabled = true }
+  enable() { this.enabled = true },
 }
 
-export { Calcmachine, CPU, Memory, GPU }
+export { Calcmachine, CPU, Memory }
